9f64643935b31ee76cb4a5e60e80903b8dfb761e
oscarspalk <oscar.gaardsted.spalk@gmail.com>
2025-04-02 13:13:53 +0200

    okay, this is a nice flow over a rectangle

--- rectangle.py
+++ rectangle.py
@@ -1,20 +1,25 @@
 import numpy
 from matplotlib import pyplot, cm
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.patches import Rectangle
+
+length_x = 40
+length_y = 40
+
 nx = 41
 ny = 41
 nt = 500
 nit = 50
 c = 1
-dx = 2 / (nx - 1)
-dy = 2 / (ny - 1)
-x = numpy.linspace(0, 2, nx)
-y = numpy.linspace(0, 2, ny)
+dx = length_x / (nx - 1)
+dy = length_y / (ny - 1)
+x = numpy.linspace(0, length_x, nx)
+y = numpy.linspace(0, length_y, ny)
 X, Y = numpy.meshgrid(x, y)
 
 rho = 1
 nu = .1
-dt = .001
+dt = .005
 
 u = numpy.zeros((ny, nx))
 v = numpy.zeros((ny, nx))
@@ -53,7 +58,7 @@ def pressure_poisson(p, dx, dy, b):
         p[0, :] = p[1, :]   # dp/dy = 0 at y = 0
         p[:, 0] = p[:, 1]   # dp/dx = 0 at x = 0
         p[-1, :] = p[0, :]  # dp/dy = 0 at y = 2
-        #p[10:20, 10:20] = 0
+        p[square_x:square_x+size, square_y:square_y+size] = 0
     return p
 
 def cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu):
@@ -108,16 +113,22 @@ def cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu):
 
 u, v, p = cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu)
 
-fig = pyplot.figure(figsize=(11,7), dpi=100)
+fig,ax = pyplot.subplots()
 # plotting the pressure field as a contour
 pyplot.contourf(X, Y, p, alpha=0.5, cmap=cm.viridis)  
 pyplot.colorbar(
     label='p'
 )
 # plotting the pressure field outlines
-pyplot.contour(X, Y, p, cmap=cm.viridis)  
+ax.set_aspect('equal')
+rectang = Rectangle(((square_x-0.5) *dx, (square_y-0.5)*dx), size*dx, size*dx)
+
+ax.add_patch(rectang)
+
+scaling = 1
+
 # plotting velocity field
-pyplot.quiver(X, Y, u, v) 
+pyplot.streamplot(X[::scaling], Y[::scaling], u[::scaling], v[::scaling], density=3) 
 pyplot.xlabel('X')
 pyplot.ylabel('Y')
 pyplot.show()
